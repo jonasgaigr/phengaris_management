4591f3af33e781f63d5afbbb5240ecaa909edc9a
Jonas Gaigr <jonas.gaigr@gmail.com>
2025-05-24 13:40:41 +0200

    get id_local on spatial filter by protected area

--- Protected_area_id.R
+++ Protected_area_id.R
@@ -1,24 +1,65 @@
-
-phengaris_evl_id <- phengaris_lokal %>%
-  sf::st_intersection(., evl) %>%
+#----------------------------------------------------------#
+# Identify occurrences within protected areas -----
+#----------------------------------------------------------#
+#--------------------------------------------------#
+## Natura 2000 both species -----
+#--------------------------------------------------#
+phengaris_evl_id <- 
+  phengaris_lokal_new %>%
+  sf::st_intersection(
+    ., 
+    evl
+    ) %>%
   sf::st_make_valid() %>%
   dplyr::pull(ID_LOKAL)
 
-phenau_evl_id <- phengaris_lokal %>%
-  sf::st_intersection(., evl %>% 
-                        dplyr::filter(SITECODE %in% filter(sites_subjects, 
-                                                           nazev_lat == "Phengaris nausithous")$site_code)) %>%
+#--------------------------------------------------#
+## Natura 2000 - P. nausithous target feature  -----
+#--------------------------------------------------#
+evl_id_Pnau <- 
+  phengaris_lokal_new %>%
+  sf::st_intersection(
+    ., 
+    evl %>%
+      dplyr::filter(
+        SITECODE %in% filter(
+          sites_subjects, 
+          nazev_lat == "Phengaris nausithous"
+          )$site_code
+        )
+    ) %>%
   sf::st_make_valid() %>%
   dplyr::pull(ID_LOKAL)
 
-phetel_evl_id <- phengaris_lokal %>%
-  sf::st_intersection(., evl %>% 
-                        dplyr::filter(SITECODE %in% filter(sites_subjects, 
-                                                           nazev_lat == "Phengaris teleius")$site_code)) %>%
+#--------------------------------------------------#
+## Natura 2000 - P. teleius target feature -----
+#--------------------------------------------------#
+evl_id_Ptel <- 
+  phengaris_lokal_new %>%
+  sf::st_intersection(
+    ., 
+    evl %>%
+      dplyr::filter(
+        SITECODE %in% filter(
+          sites_subjects, 
+          nazev_lat == "Phengaris teleius"
+        )$site_code
+      )
+  ) %>%
   sf::st_make_valid() %>%
   dplyr::pull(ID_LOKAL)
 
-phengaris_mzchu_id <- phengaris_lokal %>%
-  sf::st_intersection(., mzchu) %>%
+#--------------------------------------------------#
+## National small scale protected areas -----
+#--------------------------------------------------#
+phengaris_mzchu_id <- 
+  phengaris_lokal_new %>%
+  sf::st_intersection(
+    ., 
+    mzchu
+    ) %>%
   sf::st_make_valid() %>%
   dplyr::pull(ID_LOKAL)
+
+
+
