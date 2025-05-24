21114cafb7ef4b28a73af1a82cf9415b1212b672
Jonas Gaigr <jonas.gaigr@gmail.com>
2025-05-24 12:28:47 +0200

    get mapping fields with 2012 - 2018 occurrence

--- Baseline_distribution.R
+++ Baseline_distribution.R
@@ -7,10 +7,10 @@ data_old_sitmap_intersection <-
     sitmap
   ) %>%
   sf::st_drop_geometry()
+
 #----------------------------------------------------------#
 # Get mapping fields with occurrence -----
 #----------------------------------------------------------#
-
 #--------------------------------------------------#
 ## Get mapping fields with occurrence of P. nausithous -----
 #--------------------------------------------------#
@@ -19,21 +19,110 @@ data_old_sitmap_Pnau <-
   dplyr::filter(
     DRUH == "Phengaris nausithous"
     ) %>%
-  dplyr::group_by(SITMAP) %>%
+  dplyr::group_by(POLE) %>%
   dplyr::arrange(
     desc(
-      DATUM_DO
+      as.numeric(
+        DATUM_DO
+      )
     )
   ) %>%
   dplyr::slice(1) %>%
   dplyr::filter(
     NEGATIV == 0
-  ) %>%
-  pull(SITMAP)
+  )
 
 #--------------------------------------------------#
 ## Get mapping fields with occurrence of P. teleius -----
 #--------------------------------------------------#
+data_old_sitmap_Ptel <-
+  data_old_sitmap_intersection %>%
+  dplyr::filter(
+    DRUH == "Phengaris teleius"
+  ) %>%
+  dplyr::group_by(POLE) %>%
+  dplyr::arrange(
+    desc(
+      as.numeric(
+        DATUM_DO
+      )
+    )
+  ) %>%
+  dplyr::slice(1) %>%
+  dplyr::filter(
+    NEGATIV == 0
+  )
+
+#----------------------------------------------------------#
+# Set recent (2019 - 2019) occurrence -----
+#----------------------------------------------------------#
+
+data_new_sitmap_intersection <-
+  sf::st_intersection(
+    phengaris_lokal_new,
+    sitmap
+  ) %>%
+  sf::st_drop_geometry()
+
+#----------------------------------------------------------#
+#--------------------------------------------------#
+## Get mapping fields with occurrence of P. nausithous -----
+#--------------------------------------------------#
+data_old_sitmap_Pnau <-
+  data_old_sitmap_intersection %>%
+  dplyr::filter(
+    DRUH == "Phengaris nausithous"
+  ) %>%
+  dplyr::group_by(POLE) %>%
+  dplyr::arrange(
+    desc(
+      as.numeric(
+        DATUM_DO
+      )
+    )
+  ) %>%
+  dplyr::slice(1) %>%
+  dplyr::filter(
+    NEGATIV == 0
+  )
+
+#--------------------------------------------------#
+## Get mapping fields with occurrence of P. teleius -----
+#--------------------------------------------------#
+data_old_sitmap_Ptel <-
+  data_old_sitmap_intersection %>%
+  dplyr::filter(
+    DRUH == "Phengaris teleius"
+  ) %>%
+  dplyr::group_by(POLE) %>%
+  dplyr::arrange(
+    desc(
+      as.numeric(
+        DATUM_DO
+      )
+    )
+  ) %>%
+  dplyr::slice(1) %>%
+  dplyr::filter(
+    NEGATIV == 0
+  )
+
+#----------------------------------------------------------#
+# Get recent (2019 - 2019) number of species per field -----
+#----------------------------------------------------------#
+data_new_sitmap_intersection %>%
+  group_by(
+    POLE
+  ) %>%
+  reframe(
+    species_number = DRUH %>%
+      unique() %>%
+      length()
+  )
+
+#----------------------------------------------------------#
+# Impute recent (2019 - 2019) negative records -----
+#----------------------------------------------------------#
 
 #----------------------------------------------------------#
 # Export imputed data -----
